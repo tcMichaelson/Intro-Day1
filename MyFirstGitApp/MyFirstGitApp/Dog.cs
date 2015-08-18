229bc93e5bf8df8cabd721a80f67fb3c9b71068b
tcMichaelson <tc.michaelson@gmail.com>
2015-08-18 18:05:45 -0500

    More updates

---
+++ MyFirstGitApp/MyFirstGitApp/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitApp
+{
+    class Dog
+    {
+        // lbs
+        int weight = 100;
+        string color = "brown";
+
+        public void bark()
+        {
+            Console.WriteLine("Ruff");
+        }
+    }
+}
--- MyFirstGitApp/MyFirstGitApp/Program.cs
+++ MyFirstGitApp/MyFirstGitApp/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyFirstGitApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace MyFirstGitApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(string.Format("{0:HH:mm:ss tt}", DateTime.Now));
+            Console.WriteLine(DateTime.Now.TimeOfDay);
+            Debug.Assert(true);
+            int i = 20;
+            int j = 20;
+            i = i / 40 * 2;
+            j = (int)((j / 40) + 3);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+            string userDate = Console.ReadLine();
+            DateTime newDate = DateTime.Parse(userDate);
+            Console.WriteLine(newDate.ToString("MM/dd/yyyy"));
+            string strTwo = "This";
+            Console.WriteLine("{0} {1}", userDate, strTwo);
+            Console.ReadLine();
         }
     }
 }
